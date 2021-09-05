99eff5f5020421502e0207993afd9b4c021fc4e9
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-09-05 18:59:34 +0200

    Add PrimeVue and add menubar as example usecase

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,43 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <Menubar :model="items">
+      <template #start>
+        <h1>Rhada</h1>
+      </template>
+      <template #end>
+        <InputText placeholder="Search" type="text" />
+      </template>
+      <Menubar :model="items">
+        <template #item="{ item }">
+          <router-link :to="item.to">
+            {{ item.label }}
+          </router-link>
+        </template>
+      </Menubar>
+    </Menubar>
+    <router-view />
   </div>
-  <router-view />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { ref } from "vue";
 
-#nav {
-  padding: 30px;
-}
+export default {
+  setup() {
+    const items = ref([
+      {
+        label: "Home",
+        icon: "pi pi-fw pi-home",
+        to: "/",
+      },
+      {
+        label: "About",
+        icon: "pi pi-fw pi-question-circle",
+        to: "/about",
+      },
+    ]);
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+    return { items };
+  },
+};
+</script>
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,16 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import PrimeVue from 'primevue/config';
+import Menubar from 'primevue/menubar';
 
-createApp(App).use(store).use(router).mount("#app");
+
+const app = createApp(App)
+
+app.use(store)
+app.use(router)
+app.use(PrimeVue)
+app.component('Menubar', Menubar)
+
+app.mount("#app");
+
